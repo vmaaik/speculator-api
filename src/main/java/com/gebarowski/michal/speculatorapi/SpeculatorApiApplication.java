3b3a1243f64f3eddebc054c14d7c4ad05b80951f
Michal Gebarowski <vmaaik@gmail.com>
2019-10-16 21:49:18 +0100

    init speculator-api

---
+++ src/main/java/com/gebarowski/michal/speculatorapi/SpeculatorApiApplication.java
@@ -0,0 +1,13 @@
+package com.gebarowski.michal.speculatorapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpeculatorApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpeculatorApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gebarowski/michal/speculatorapi/SpeculatorApiApplicationTests.java
@@ -0,0 +1,16 @@
+package com.gebarowski.michal.speculatorapi;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpeculatorApiApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
