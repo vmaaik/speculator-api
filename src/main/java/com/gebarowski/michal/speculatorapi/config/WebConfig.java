29244fd3d93dc48e1af37f6d937f4ff713fdc755
michal gebarowski <vmaaik@gmail.com>
2020-05-10 14:07:38 +0100

    fix exchange rate endpoint

--- src/main/java/com/gebarowski/michal/speculatorapi/config/WebConfig.java
+++ src/main/java/com/gebarowski/michal/speculatorapi/config/WebConfig.java
@@ -1,11 +1,20 @@
 package com.gebarowski.michal.speculatorapi.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @EnableWebMvc
 @Configuration
+@PropertySource("classpath:application.properties")
 @ComponentScan("com.gebarowski.michal.speculatorapi")
 public class WebConfig {
+
+    @Bean
+    public static PropertySourcesPlaceholderConfigurer propertySourcesPlaceholderConfigurer () {
+        return new PropertySourcesPlaceholderConfigurer();
+    }
 }
